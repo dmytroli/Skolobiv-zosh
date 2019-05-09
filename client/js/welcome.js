0e3b021983676e6bb3df20999c3385d2b063249d
Dmytro <volodavoloda380@ukr.net>
2019-05-09 09:33:50 +0300

    Модифіковані файли і додані фото до слайдерів більшості кабінетів. Додані нові фото. Завершений процес автоматизації: gulpfile.js

---
+++ client/js/welcome.js
@@ -0,0 +1,1 @@
+var welButton=document.querySelector(".wel-button-mobile"),welcomeMenu=document.querySelector(".welcome_list");welButton.addEventListener("click",function(){welcomeMenu.classList.contains("menu-mobile--close")?welcomeMenu.classList.remove("menu-mobile--close"):welcomeMenu.classList.add("menu-mobile--close")});
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass'); /*препроцесор*/
-var plumber = require('gulp-plumber'); /*допомагає шукати помилки в препроцерному коді*/
+var plumber = require('gulp-plumber'); /*допомагає шукати помилки в препроцерному коді і не припиняє астоматизацію*/
 var autoprefixer = require('autoprefixer'); /*астоматичне додавання префіксів в css*/
 var postcss = require('gulp-postcss'); /*для роботи автопрефіксера*/
 var browserSync = require('browser-sync').create(); /*для локального сервера*/
@@ -14,15 +14,25 @@ var posthtml = require('gulp-posthtml');
 var include = require('posthtml-include');
 var minjs = require('gulp-uglify'); /*мініфікація js*/
 var webp = require('gulp-webp');
+var clean = require('gulp-clean');
 
 
 
-gulp.task('html', function() {
-  return gulp.src('index.html')
-    .pipe(posthtml([
-      include()
-      ]))
-    .pipe(gulp.dest('../www/')) 
+gulp.task('clean', function () {
+    return gulp.src('client/*', {read: false})
+        .pipe(clean());
+});
+
+
+/*інлайнить svg в html документи
+  <div style="display: none">
+    <include src="img/sprite.svg"></include>  
+  </div>
+*/
+gulp.task('phtml', function() {
+  return gulp.src('../www/*.html')
+    .pipe(posthtml([include()]))
+    .pipe(gulp.dest('client/')) 
 });
 
 
@@ -36,7 +46,7 @@ gulp.task('sass', function() {
         }) ]))
    .pipe(gulp.dest('css'))
    .pipe(minify())                             /*мініфікація css*/
-   .pipe(rename("style.min.css"))              /*створення нового файлу*/
+                /*створення нового файлу*/
    .pipe(gulp.dest('css'))                     /*запис в новий файл мініфікованого */
    .pipe(browserSync.stream());
 });
@@ -45,37 +55,38 @@ gulp.task('sass', function() {
 gulp.task('minjs', function() {
    return gulp.src('js/*.js')
    .pipe(minjs())                         
-   .pipe(gulp.dest('js/min.js/'))                     
+   .pipe(gulp.dest('client/js'))                     
 });
 
-/*імітація роботи сервера*/
+
+//імітація роботи сервера контролюються всі зміни в усіх файлах
 gulp.task('br', function() {   
-    browserSync.init({
-      server: {
-        baseDir: '../www/'
-        },
-        notify: false
-    });
+    browserSync.init({server: '.'});
+    gulp.watch('css/**/*.scss', gulp.series('sass'));
+    gulp.watch('../www/*.html').on('change', browserSync.reload);
+    gulp.watch('css/*.scss').on('change', browserSync.reload);
 });
+
+
 
 /*створення svg спрайту*/
 gulp.task('svg', function() {                   
     return gulp.src('img/*.svg')
         .pipe(svgstore({ inlineSvg: true }))
         .pipe(rename("sprite.svg"))
-        .pipe(gulp.dest('img'))
+        .pipe(gulp.dest('client/img'))
 });
 
 /*мініфікація зображень*/
 gulp.task('images', function() {                   
-    return gulp.src('img/**/*.{png,svg}')
+    return gulp.src('client/img/**/*.{png,svg}')
         .pipe(imagemin(
           [
           imagemin.jpegtran({progressive: true}),
           imagemin.optipng({optimizationLevel: 3}),
           imagemin.svgo()
           ]))
-        .pipe(gulp.dest('img'))
+        .pipe(gulp.dest('client/img/'))
 });
 
 /*перетворенння в webp*/
@@ -85,30 +96,33 @@ gulp.task('webp', function() {
         .pipe(gulp.dest('img'))
 });
 
-/*запуск основних функцій для розробки*/
-gulp.task('watch', function() { 
-	gulp.watch('./css/**/*.scss', gulp.series('sass'));
-  gulp.watch('../www/*.html')
-  .on('change', browserSync.reload);
-});
 
-/*запуск task один за одним*/
-gulp.task('develop', gulp.series('sass', 'images', 'minjs')) 
+
+
+/*запуск основних функцій для розробки (не працює)*/
+//gulp.task('watc', function() { 
+	//gulp.watch('./css/**/*.scss', gulp.series('sass'));
+  //gulp.watch('../www/*.html')
+  //.on('change', browserSync.reload);
+//});
 
 
 /*копіювання тільки необхідних файлів в потрібну папку для передачі клієнту*/
 gulp.task('copy', function() {                 
     return gulp.src([
-      "../www/font/*.ttf",
-      "../www/img/**",
-      "../www/js/min.js/*.js",
-      "../www/css/*.css",
+      "fonts/*.ttf",
+      "img/**",
+      "js/*.js",
+      "css/*.css",
       "*.html"
       ],
-      {base: '../www'}
+      {base: '.'}
       )
       .pipe(gulp.dest('client'))
 });
+
+/*запуск task один за одним: очищення, копіювання, перетворення в css, автопрефікси, мініфікація js, спрайт svg, мініфікація зображень, інлайн svg в html*/
+gulp.task('develop', gulp.series('clean', 'copy', 'sass', 'minjs', 'svg', 'images', 'phtml')); 
 
 
 
